7e5010fc0afdf05bb0ec3e7cfb08e4c71f0312a2
jermaine joseph <105750913+Jermaine1988@users.noreply.github.com>
2022-06-06 17:50:58 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let emailElement = document.querySelector('#email');
+let messageElement = document.querySelector ('#message');
+
+let submitButton = document.querySelector('#submit-button');
+
+if(emailValue.includes) {
+
+    alert('thank you for your message');
+} else {
+    alert('Oops try again my boy. Wrong email address');
+}
